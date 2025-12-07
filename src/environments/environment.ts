ba3d90f3b35c929c2bab44abfc2c71d1b40e282d
Marco Antonio Chavez Baltierrez <marco1102004@gmail.com>
2025-12-07 17:52:57 -0600

    cambiando url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 //const baseURL = 'http://127.0.0.1:8000/api/';
-const baseURL = 'https://ungangrened-filomena-epixylous.ngrok-free.dev/api/';
+const baseURL = 'https://hw-api.on-forge.com/api/';
+
 export const environment = {
   version: '1.0.0',
   versionCheck: `${baseURL}version`,
