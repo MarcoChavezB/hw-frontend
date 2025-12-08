d4861487610726c0caf8dfe680036257befc026a
Marco <marco.aviasis@gmail.com>
2025-12-08 13:11:33 -0600

    cabiando comentarios

--- src/app/components/cards/post-card/post-card.component.ts
+++ src/app/components/cards/post-card/post-card.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, inject, input, Input, OnInit, Output } from '@angular/core';
-import { IonItem, IonButton, IonIcon, IonCard, ModalController ,IonAvatar, IonLabel, IonChip, IonCardContent, IonImg, IonGrid, IonRow, IonCol, IonItemDivider } from "@ionic/angular/standalone";
+import { IonItem, IonButton, IonIcon, IonCard, ModalController, IonAvatar, IonLabel, IonChip, IonCardContent, IonImg, IonGrid, IonRow, IonCol, IonItemDivider } from "@ionic/angular/standalone";
 import { Directory, Filesystem } from '@capacitor/filesystem';
 import { FormsModule } from '@angular/forms';
 import { CommentsModalComponent } from '../comments-modal/comments-modal.component';
@@ -9,139 +9,138 @@ import { AuthService } from 'src/app/services/auth/auth-service';
 import { UnlogModalComponent } from '../unlog-modal/unlog-modal.component';
 
 @Component({
-  selector: 'app-post-card',
-  templateUrl: './post-card.component.html',
-  styleUrls: ['./post-card.component.scss'],
-  imports: [IonCardContent, IonChip, IonLabel, IonAvatar, IonButton, IonCard, IonItem, CommonModule, FormsModule],
+    selector: 'app-post-card',
+    templateUrl: './post-card.component.html',
+    styleUrls: ['./post-card.component.scss'],
+    imports: [IonCardContent, IonChip, IonLabel, IonAvatar, IonButton, IonCard, IonItem, CommonModule, FormsModule],
 })
 export class PostCardComponent {
-  constructor(
-    private modalCtrl : ModalController
-  ) { }
-  @Input() postId: number = 0;
-  @Input() avatar: string = '';
-  @Input() username: string = '';
-  @Input() location: string = '';
-  @Input() image: string = '';
-  @Input() content: string = '';
-  @Input() likes : number = 0;
-  @Input() comments : number = 0;
-  @Input() favorites : number = 0;
-  @Input() hashtags: string[] = []; 
-  @Input() title: string = '';
-  @Input() liked : boolean = false;
-  @Input() favorite : boolean = false;
-  @Input() commentsData: Comment[] = [];
-  @Input() createdAt: string = '';
-   
-  @Output() likeEvent = new EventEmitter<boolean>();
-  @Output() favoriteEvent = new EventEmitter<boolean>();
-  @Output() showProfileEvent = new EventEmitter<void>();
+    constructor(
+        private modalCtrl: ModalController
+    ) { }
+    @Input() postId: number = 0;
+    @Input() avatar: string = '';
+    @Input() username: string = '';
+    @Input() location: string = '';
+    @Input() image: string = '';
+    @Input() content: string = '';
+    @Input() likes: number = 0;
+    @Input() comments: number = 0;
+    @Input() favorites: number = 0;
+    @Input() hashtags: string[] = [];
+    @Input() title: string = '';
+    @Input() liked: boolean = false;
+    @Input() favorite: boolean = false;
+    @Input() commentsData: Comment[] = [];
+    @Input() createdAt: string = '';
 
-  localImage: string = ''; 
+    @Output() likeEvent = new EventEmitter<boolean>();
+    @Output() favoriteEvent = new EventEmitter<boolean>();
+    @Output() showProfileEvent = new EventEmitter<void>();
 
-  authService = inject(AuthService);
-  
-  async ngOnInit() {
-    if (this.image) {
-      await this.loadImage(this.image);
-    }
-  }
+    localImage: string = '';
 
-async loadImage(url: string) {
-  try {
-    const filename = this.hashCode(url) + '.jpg';
+    authService = inject(AuthService);
 
-    const file = await Filesystem.readFile({
-      path: filename,
-      directory: Directory.Data
-    }).catch(() => null);
-
-    if (file) {
-      this.localImage = `data:image/jpeg;base64,${file.data}`;
-      return;
+    async ngOnInit() {
+        if (this.image) {
+            await this.loadImage(this.image);
+        }
     }
 
-    const response = await fetch(url);
-    const blob = await response.blob();
-    const base64 = (await this.convertBlobToBase64(blob)) as string;
-    const base64Data = base64.split(',')[1];
+    async loadImage(url: string) {
+        try {
+            const filename = this.hashCode(url) + '.jpg';
 
-    await Filesystem.writeFile({
-      path: filename,
-      data: base64Data,
-      directory: Directory.Data
-    });
+            const file = await Filesystem.readFile({
+                path: filename,
+                directory: Directory.Data
+            }).catch(() => null);
 
-    this.localImage = `data:image/jpeg;base64,${base64Data}`;
-  } catch (error) {
-    console.warn('No se pudo descargar la imagen, se mostrará local si existe:', error);
-  }
-}
+            if (file) {
+                this.localImage = `data:image/jpeg;base64,${file.data}`;
+                return;
+            }
 
+            const response = await fetch(url);
+            const blob = await response.blob();
+            const base64 = (await this.convertBlobToBase64(blob)) as string;
+            const base64Data = base64.split(',')[1];
 
-  convertBlobToBase64(blob: Blob): Promise<string | ArrayBuffer | null> {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.onerror = reject;
-      reader.readAsDataURL(blob);
-    });
-  }
+            await Filesystem.writeFile({
+                path: filename,
+                data: base64Data,
+                directory: Directory.Data
+            });
 
-  hashCode(str: string): string {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = (hash << 5) - hash + str.charCodeAt(i);
-      hash |= 0;
+            this.localImage = `data:image/jpeg;base64,${base64Data}`;
+        } catch (error) {
+            console.warn('No se pudo descargar la imagen, se mostrará local si existe:', error);
+        }
     }
-    return hash.toString();
-  }
-  
-  
-  async like(){
-    if(!await this.authService.isAuthenticated()){
-      await this.openLoginPrompt();
-      return;
+
+
+    convertBlobToBase64(blob: Blob): Promise<string | ArrayBuffer | null> {
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+        });
     }
-    this.likeEvent.emit(this.liked);
-  }
-  
-  async fav(){
-    if(!await this.authService.isAuthenticated()){
-      await this.openLoginPrompt();
-      return;
+
+    hashCode(str: string): string {
+        let hash = 0;
+        for (let i = 0; i < str.length; i++) {
+            hash = (hash << 5) - hash + str.charCodeAt(i);
+            hash |= 0;
+        }
+        return hash.toString();
     }
-    this.favoriteEvent.emit(this.favorite);
-  }
-  
+
+
+    async like() {
+        if (!await this.authService.isAuthenticated()) {
+            await this.openLoginPrompt();
+            return;
+        }
+        this.likeEvent.emit(this.liked);
+    }
+
+    async fav() {
+        if (!await this.authService.isAuthenticated()) {
+            await this.openLoginPrompt();
+            return;
+        }
+        this.favoriteEvent.emit(this.favorite);
+    }
+
     async openLoginPrompt() {
-      const modal = await this.modalCtrl.create({
-        component: UnlogModalComponent,
-        cssClass: 'login-prompt-modal',
-        animated: true,
-        backdropDismiss: true,
-        initialBreakpoint: 0.25,
-        breakpoints: [0, 0.25]
-      });
-    
-      await modal.present();
+        const modal = await this.modalCtrl.create({
+            component: UnlogModalComponent,
+            cssClass: 'login-prompt-modal',
+            animated: true,
+            backdropDismiss: true,
+            initialBreakpoint: 0.25,
+            breakpoints: [0, 0.25]
+        });
+
+        await modal.present();
     }
-  
-    showProfile(){
-      this.showProfileEvent.emit();
+
+    showProfile() {
+        this.showProfileEvent.emit();
     }
-  
-  async openCommentsModal(){
-    const modal = await this.modalCtrl.create({
-        component: CommentsModalComponent,
-          presentingElement: document.querySelector('ion-router-outlet')!,
-          cssClass: 'comments-modal',
-          componentProps: {
-            comments: this.commentsData,
-            postId: this.postId
-          }
-    });
-    await modal.present();
-  }
+
+    async openCommentsModal() {
+        const modal = await this.modalCtrl.create({
+            component: CommentsModalComponent,
+            presentingElement: document.querySelector('ion-router-outlet')!,
+            componentProps: {
+                comments: this.commentsData,
+                postId: this.postId,
+            },
+        });
+        await modal.present();
+    }
 }
--- src/app/pages/principal/fyp/fyp.component.ts
+++ src/app/pages/principal/fyp/fyp.component.ts
@@ -1,102 +1,105 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonContent, RefresherCustomEvent ,IonList, IonItem, ToastController, IonLabel, IonInfiniteScroll, IonInfiniteScrollContent, InfiniteScrollCustomEvent, IonProgressBar, IonRefresherContent, IonRefresher } from "@ionic/angular/standalone";
+import { IonContent, RefresherCustomEvent, IonList, IonItem, ToastController, IonLabel, IonInfiniteScroll, IonInfiniteScrollContent, InfiniteScrollCustomEvent, IonProgressBar, IonRefresherContent, IonRefresher } from "@ionic/angular/standalone";
 import { PostCardComponent } from "src/app/components/cards/post-card/post-card.component";
 import { Post } from 'src/app/models/Post';
 import { DataService } from 'src/app/services/data/data-service';
 import { PostService } from 'src/app/services/post/post-service';
 
 @Component({
-  selector: 'app-fyp',
-  templateUrl: './fyp.component.html',
-  styleUrls: ['./fyp.component.scss'],
-  imports: [IonRefresher, IonRefresherContent, IonList, IonContent, PostCardComponent, CommonModule, IonItem],
+    selector: 'app-fyp',
+    templateUrl: './fyp.component.html',
+    styleUrls: ['./fyp.component.scss'],
+    imports: [IonRefresher, IonRefresherContent, IonList, IonContent, PostCardComponent, CommonModule, IonItem],
 })
-export class FypComponent  implements OnInit {
-  posts : Post[] = [];
-  postService = inject(PostService);
-  dataService = inject(DataService);
-  router = inject(Router);
-  
-  ngOnInit() {
-    this.fetchPosts();
-  }
-  
-  constructor(
-  private toastCtrl: ToastController
-  ){}
-  
-  async fetchPosts() {
-    this.posts = await this.dataService.obtenerPostData() || [];
-    try {
-      const response = await this.postService.getPosts().toPromise();
-      if (!response!.success || response!.data.length === 0) {
-        this.posts = [];
-        await this.dataService.clearPostData();
-      } else {
-        this.toatsMessage('Posts actualizados.');
-        this.posts = response!.data;
-        await this.dataService.guardarPostData(response!.data);
-      }
-    } catch (error) {
-      this.toatsMessage('Estas viendo posts sin conexión.');
-      this.posts = await this.dataService.obtenerPostData() || [];
-    }
-  }
-  
-  toatsMessage(message: string){
-    this.toastCtrl.create({
-        position: 'top',
-        message: message,
-        duration: 3000,
-      }).then(toast => toast.present());
-  }
-  
-  onIonInfinite(event: InfiniteScrollCustomEvent) {
-  }
-  
-  likePost(postId: number, post : Post){
-    post.already_liked = !post.already_liked;
-    post.likes.length += post.already_liked ? 1 : -1;
-    this.postService.toggleLikePost(postId).subscribe({
-        next: (response) => {
-            post.already_liked = response.state;
-        },
-        error: (error) => {
-            console.error('Error toggling like:', error);
-            post.already_liked = !post.already_liked;
-            post.likes.length += post.already_liked ? 1 : -1;
-        }
-    });
-  }
-  
-  savePost(postId: number, post: Post){
-    post.already_saved = !post.already_saved;
-    post.saves.length += post.already_saved ? 1 : -1;
-    this.postService.toggleSavePost(postId).subscribe({
-        next: (response) => {
-            console.log('Post save state toggled:', response);
-        },
-        error: (error) => {
-            console.error('Error toggling save:', error);
-            post.already_saved = !post.already_saved;
-            post.saves.length += post.already_saved ? 1 : -1;
-        }
-    });
-  }
+export class FypComponent implements OnInit {
+    posts: Post[] = [];
+    postService = inject(PostService);
+    dataService = inject(DataService);
+    router = inject(Router);
 
-   selectedProfile(userId: number){ {
-        this.router.navigate([`/home/profile/${userId}`], { 
-          replaceUrl: false, 
-          state: { animation: { direction: 'forward' } } 
+    ngOnInit() {
+        this.fetchPosts();
+    }
+
+    constructor(
+        private toastCtrl: ToastController
+    ) { }
+
+    async fetchPosts() {
+        this.posts = await this.dataService.obtenerPostData() || [];
+        try {
+            const response = await this.postService.getPosts().toPromise();
+
+            if (response && response.success && response.data.length > 0) {
+                this.posts = response.data;
+                await this.dataService.guardarPostData(response.data);
+                this.toastMessage('Posts actualizados.');
+            } else {
+                this.posts = [];
+                await this.dataService.clearPostData();
+                this.toastMessage('No hay nuevos posts disponibles.');
+            }
+        } catch (error) {
+            this.toastMessage('Estas viendo posts sin conexión.');
+            this.posts = await this.dataService.obtenerPostData() || [];
+        }
+    }
+
+    toastMessage(message: string) {
+        this.toastCtrl.create({
+            position: 'top',
+            message: message,
+            duration: 3000,
+        }).then(toast => toast.present());
+    }
+
+    onIonInfinite(event: InfiniteScrollCustomEvent) {
+    }
+
+    likePost(postId: number, post: Post) {
+        post.already_liked = !post.already_liked;
+        post.likes.length += post.already_liked ? 1 : -1;
+        this.postService.toggleLikePost(postId).subscribe({
+            next: (response) => {
+                post.already_liked = response.state;
+            },
+            error: (error) => {
+                console.error('Error toggling like:', error);
+                post.already_liked = !post.already_liked;
+                post.likes.length += post.already_liked ? 1 : -1;
+            }
         });
     }
-   }
-   
-  handleRefresh(event: RefresherCustomEvent) {
-    this.fetchPosts().then(() => {
-      event.target.complete();
-    }); 
-  }
+
+    savePost(postId: number, post: Post) {
+        post.already_saved = !post.already_saved;
+        post.saves.length += post.already_saved ? 1 : -1;
+        this.postService.toggleSavePost(postId).subscribe({
+            next: (response) => {
+                console.log('Post save state toggled:', response);
+            },
+            error: (error) => {
+                console.error('Error toggling save:', error);
+                post.already_saved = !post.already_saved;
+                post.saves.length += post.already_saved ? 1 : -1;
+            }
+        });
+    }
+
+    selectedProfile(userId: number) {
+        {
+            this.router.navigate([`/home/profile/${userId}`], {
+                replaceUrl: false,
+                state: { animation: { direction: 'forward' } }
+            });
+        }
+    }
+
+    handleRefresh(event: RefresherCustomEvent) {
+        this.fetchPosts().then(() => {
+            event.target.complete();
+        });
+    }
 }
